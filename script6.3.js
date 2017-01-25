0c5d5c32f8d82c0d97641f0c5d168431f4f0a5b7
Konika13 <konika.agarwal1994@gmail.com>
2017-01-25 14:55:54 +0530

    Add files via upload

---
+++ script6.3.js
@@ -0,0 +1,18 @@
+function first(){
+var sum=0;
+for(var i=0; i<=1000;i++)
+{
+sum+=i;
+}
+console.log(sum)
+}
+
+function second(){
+var add=0;
+var i=0;
+while(i<=1000){
+add+=i;
+i++;
+}
+console.log(add);
+}
